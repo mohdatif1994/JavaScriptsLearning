ce41463ef47f5455430c90f7f3e4c6791ec922ee
Mohd Atif Khan <mohd.atif68@gmail.com>
2024-07-14 16:38:35 +0530

    Tic Tac Toe implemented

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,7 +1,12 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGame = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
 
 let turn0 = true; // player 0 turn
+let count = 0; // to track draw
 
 let winPatterns = [
     [0, 1, 2],
@@ -11,13 +16,19 @@ let winPatterns = [
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
 ];
+const resetGame = () => {
+    turnO = true;
+    count = 0;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+  };
 
 boxes.forEach((box) => {
-    box.addEventListener("click", () =>{
+    box.addEventListener("click", () => {
         console.log("box was clicked")
-        if (turn0){
+        if (turn0) {
             box.innerText = "0";
             turn0 = false;
         }
@@ -26,9 +37,23 @@ boxes.forEach((box) => {
             turn0 = true;
         }
         box.disabled = true;
-        checkWinner();
+        let isWinner = checkWinner();
+        count++;
+        if (count === 9 && !isWinner)
+            {
+                gameDraw();
+            }
     });
 });
+
+newGame.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
+const gameDraw = () => {
+    msg.innerText = `Game was a Draw.`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+  };
 
 const showWinner = (winner) => {
     msg.innerText = `Congratulations, Winner is ${winner}`;
@@ -36,14 +61,28 @@ const showWinner = (winner) => {
     disableBoxes();
 }
 
+const disableBoxes = () => {
+    for (let box of boxes) {
+      box.disabled = true;
+    }
+  };
+
+  const enableBoxes = () => {
+    for (let box of boxes) {
+      box.disabled = false;
+      box.innerText = "";
+    }
+  };
+
 const checkWinner = () => {
     for (let pattern of winPatterns) {
         let post1 = boxes[pattern[0]].innerText;
-        let post2 = boxes[pattern[0]].innerText;
-        let post3 = boxes[pattern[0]].innerText;
+        let post2 = boxes[pattern[1]].innerText;
+        let post3 = boxes[pattern[2]].innerText;
         if (post1 != "" && post2 != "" && post3 != ""){
-            if ((post1 == post2) && (post2 == post3) ){
+            if (post1 === post2 && post2 === post3 ){
                 showWinner(post1);
+                return true;
             }
         }
     }
